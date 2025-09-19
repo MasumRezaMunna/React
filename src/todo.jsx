2e3de75dd01dfea16b434d3951a2b64696492b44
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-19 11:36:33 +0600

    six ways to do conditional rendering : if, Ternary and AND OR

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,5 +1,5 @@
 // export default function ToDo ({task, isDone}){
-    
+
 //     return (
 //         <li>Task: {task}</li>
 //     )
@@ -14,9 +14,41 @@
 //     }
 // }
 
-export default function ToDo ({task, isDone, time = 0}){
-    if(isDone === true){
-        return <li>Done: {task} Duration: {time}</li>
-    }
-   return <li>To be Done: {task}</li>
+// export default function ToDo ({task, isDone, time = 0}){
+//     if(isDone === true){
+//         return <li>Done: {task} Duration: {time}</li>
+//     }
+//    return <li>To be Done: {task}</li>
+// }
+
+// conditional rendering: 3 ternary
+// condition ? true : false
+
+// export default function ToDo ({task, isDone, time = 0}){
+//     return isDone ? <li>Done:{task} time: {time}</li> : <li>Not Done: {task}</li>
+// }
+
+// conditional rendering : 4 &&
+
+// export default function ToDo({ task, isDone, time = 0 }) {
+//   return isDone && <li>Done Tasks: {task} time: {time}</li>
+// }
+// conditional rendering : 5 ||
+
+// export default function ToDo({ task, isDone, time = 0 }) {
+//   return isDone || <li>Not Done Tasks: {task} time: {time}</li>
+// }
+
+// conditional rendering: 6 use variable
+
+export default function ToDo({ task, isDone, time }) {
+  const displayTime = time ? time : 100;
+  let listItem;
+
+  if (isDone === true) {
+    listItem = <li>Done: {task} time: {displayTime}</li>;
+  } else {
+    listItem = <li>Pending: {task}</li>;
+  }
+  return listItem;
 }
