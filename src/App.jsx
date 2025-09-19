b3702c0003ff45b0ad6951475bec3d19466649f6
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-19 08:44:46 +0600

    prop types and how to pass or read a prop

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+// import React from "/react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>React Core Concepts</h1>
+      <Student></Student>
+      <Student></Student>
+      <Person></Person>
+      <Developer name="Mozumder" tech="JS"></Developer>
+      <Developer name="Rajon" tech="python"></Developer>
+      <Developer name="Shabana" tech="Java"></Developer>
+      <Player name="tamim" runs="5000"></Player>
+      <Player name="mushi" runs="2000"></Player>
+      <Salami event="Roja Eid" amount = "20"></Salami>
+      <Salami event="Graduation"></Salami>
+    </>
+  );
+}
+
+function Salami({event, amount = 0}) {
+  return (
+    <div className="student">
+      <p>Salami For: {event}</p>
+      <p>Amount {amount}</p>
+    </div>
+  );
+}
+
+// const {name, runs} = {name: 'tamim', runs: '5000'}
+
+function Player({ name, runs }) {
+  return (
+    <div className="student">
+      <h3>Name : {name}</h3>
+      <p>Runs : {runs}</p>
+    </div>
+  );
+}
+
+// const {name, tech} = {name: "Mozumder", tech: 'JS'}
+
+function Developer({ name, tech }) {
+  return (
+    <div
+      style={{
+        border: "2px solid green",
+        borderRadius: "20px",
+      }}
+    >
+      <h3>Developer: {name}</h3>
+      <p>Technology: {tech}</p>
+    </div>
+  );
+}
+
+function Person() {
+  const age = 17;
+  const name = "Jolil";
+  const personStyle = {
+    color: "red",
+    textAlign: "right",
+  };
+  return (
+    <p id="" title="tooltip" style={personStyle}>
+      I am a person: {name} {age}
+    </p>
+  );
+}
+
+function Student() {
+  return (
+    <div className="student">
+      <p>Name:</p>
+      <p>Dept: </p>
+    </div>
+  );
+}
+export default App;
