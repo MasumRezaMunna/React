e5b2d37b62f524b0b79cb198720d56b4031ba89e
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-29 08:23:57 +0600

    display countries in a simple way

--- react-world-on-the-go/src/components/Countries/Countries.jsx
+++ react-world-on-the-go/src/components/Countries/Countries.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { use } from "react";
 
-const Countries = () => {
-    return (
-        <div>
-            <h1>In the countries</h1>
-        </div>
-    );
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries
+  console.log(countries);
+  return (
+    <div>
+      <h1>In the countries: {countries.length}</h1>
+    </div>
+  );
 };
 
 export default Countries;
