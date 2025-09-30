665cfbb6f62318ded4e3af1989e5dbde6ae566b3
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-30 11:08:06 +0600

    lift up state in arrays by comparing reference

--- react-world-on-the-go/src/components/Country/Country.jsx
+++ react-world-on-the-go/src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './Country.css'
 
-const Country = ({ country, handleVisitedCountries }) => {
+const Country = ({ country, handleVisitedCountries, handleVisitedFlag }) => {
   const [visited, setVisited] = useState(false)
 
   // console.log(country.area);
@@ -24,13 +24,14 @@ const Country = ({ country, handleVisitedCountries }) => {
   return (
     // <div className={`country border-lg text-center ${visited ? 'country-visited' : 'country-not-visited'}`}>
     <div className={`country ${visited && 'country-visited'}`}>
-        <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+        <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
       <h3>Name: {country.name.common}</h3>
       <p>Population: {country.population.population}</p>
       <p>Area: {country.area.area} {country.area.area > 300000 ? "Big country" : "Small country"}</p>
       <button onClick={handleVisited}>
         {visited ? 'Visited' : 'Not Visited'}
       </button>
+      <button onClick= {() => {handleVisitedFlag(country?.flags?.flags?.png)}}>Add Visited Flag</button>
     </div>
   );
 };
