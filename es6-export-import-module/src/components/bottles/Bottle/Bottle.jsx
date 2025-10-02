22c05f58129dc071d527550d49c79c5e69c0cd3c
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-10-02 14:13:33 +0600

    load bottles data and show in grid layout

--- es6-export-import-module/src/components/bottles/Bottle/Bottle.jsx
+++ es6-export-import-module/src/components/bottles/Bottle/Bottle.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Bottles.css'
 
-const Bottle = () => {
+const Bottle = ({bottle, handleAddToCart}) => {
+    const {img, name, price, brand} = bottle;
+    // console.log(bottle)
     return (
-        <div>
-            
+        <div className='card bottle'>
+            <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p>{brand}</p>
+            <p>${price}</p>
+            <button onClick={()=>handleAddToCart(bottle)}>Buy Now</button>
         </div>
     );
 };
