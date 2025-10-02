300671ee1ea263730f0ebab4254022910c9d3926
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-10-02 15:09:10 +0600

    introduction to local storage : storing data in the browser

--- es6-export-import-module/src/components/bottles/Bottles.jsx
+++ es6-export-import-module/src/components/bottles/Bottles.jsx
@@ -2,18 +2,21 @@ import React, { use, useState } from "react";
 import Bottle from "./Bottle/Bottle";
 
 const Bottles = ({ bottlesPromise }) => {
-  const [cart, setCart] = useState();
+  const [cart, setCart] = useState([]);
 
   const bottles = use(bottlesPromise);
 
   const handleAddToCart = (bottle) => {
-    console.log("bottle will be added to the cart", bottle);
+    // console.log("bottle will be added to the cart", bottle);
+    const newCart = [...cart, bottle]
+    setCart(newCart)
   };
 
   //   console.log(bottles)
   return (
     <div>
       <h3>Bottles: {bottles.length}</h3>
+      <p>Added to cart: {cart.length}</p>
       <div className="bottles-container">
         {bottles.map((bottle) => (
           <Bottle
