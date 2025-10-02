6c9909cbafe98d0de87e806d38de93d806f57ac2
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-10-02 13:01:23 +0600

    load fake data, online api

--- es6-export-import-module/src/utils/math/math.js
+++
@@ -1,6 +0,0 @@
-const add = (a , b) => a + b;
-const diff = (a, b) => a - b;
-const multiplyTwoNumbers = (a, b) => a * b;
-const divide = (a, b) => a / b;
-
-export {add, diff, multiplyTwoNumbers as mult}
--- es6-export-import-module/src/utils/math/tax/tax.js
+++
@@ -1,3 +0,0 @@
-import { mult } from "../math";
-
-const calculate = mult(2000, 0.15);
